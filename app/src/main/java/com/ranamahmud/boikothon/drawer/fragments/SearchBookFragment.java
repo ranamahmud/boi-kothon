1c28a87d58a7e69b9ee7ebf61079e4d207e83548
Md Rana Mahmud <ranamd0@gmail.com>
2019-03-23 18:36:17 +0600

    Search Fragment Spinner fixed

--- app/src/main/java/com/ranamahmud/boikothon/drawer/fragments/SearchBookFragment.java
+++ app/src/main/java/com/ranamahmud/boikothon/drawer/fragments/SearchBookFragment.java
@@ -3,7 +3,9 @@ package com.ranamahmud.boikothon.drawer.fragments;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.paging.PagedList;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,11 +13,22 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.firebase.ui.firestore.paging.FirestorePagingAdapter;
+import com.firebase.ui.firestore.paging.FirestorePagingOptions;
+import com.google.firebase.firestore.Query;
 import com.ranamahmud.boikothon.R;
 import com.ranamahmud.boikothon.drawer.fragments.dummy.DummyContent;
 import com.ranamahmud.boikothon.drawer.fragments.dummy.DummyContent.DummyItem;
+import com.ranamahmud.boikothon.model.Book;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -63,6 +76,59 @@ public class SearchBookFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_searchbook_list, container, false);
 
+        // select options spinner
+        String[] fiction=getResources().getStringArray(R.array.fiction);
+        String [] nonFiction = getResources().getStringArray(R.array.non_fiction);
+        ArrayList<String> bookType = new ArrayList<String>();
+        bookType.add("All");
+        bookType.addAll(Arrays.asList(fiction));
+        bookType.addAll(Arrays.asList(nonFiction));
+        final Spinner spinnerGenre =  view.findViewById(R.id.spinnerGenresSearch);
+        ArrayAdapter<String> adapterSpinner = new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_spinner_item, bookType);
+
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGenre.setAdapter(adapterSpinner);
+        // spinner end
+
+        // query
+        // The "base query" is a query with no startAt/endAt/limit clauses that the adapter can use
+// to form smaller queries for each page.  It should only include where() and orderBy() clauses
+        Query baseQuery = mItemsCollection.orderBy("value", Query.Direction.ASCENDING);
+
+// This configuration comes from the Paging Support Library
+// https://developer.android.com/reference/android/arch/paging/PagedList.Config.html
+        PagedList.Config config = new PagedList.Config.Builder()
+                .setEnablePlaceholders(false)
+                .setPrefetchDistance(10)
+                .setPageSize(20)
+                .build();
+
+// The options for the adapter combine the paging configuration with query information
+// and application-specific options for lifecycle, etc.
+        FirestorePagingOptions<Book> options = new FirestorePagingOptions.Builder<Book>()
+                .setLifecycleOwner(this)
+                .setQuery(baseQuery, config, Book.class)
+                .build();
+
+        FirestorePagingAdapter<Book, BookViewHolder> adapter =
+                new FirestorePagingAdapter<Book, BookViewHolder>(options) {
+                    @NonNull
+                    @Override
+                    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        // Create the BookViewHolder
+                        // ...
+                    }
+
+                    @Override
+                    protected void onBindViewHolder(@NonNull BookViewHolder holder,
+                                                    int position,
+                                                    @NonNull Book model) {
+                        // Bind the Book to the view holder
+                        // ...
+                    }
+                };
+
         // Set the adapter
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
@@ -109,4 +175,35 @@ public class SearchBookFragment extends Fragment {
         // TODO: Update argument type and name
         void onListFragmentInteraction(DummyItem item);
     }
+
+    // view holder
+    public class BookViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final ImageView mImageBook;
+        public final TextView mTitle;
+        public final TextView mAuthor;
+        public final RatingBar mBookRating;
+        public final TextView mGenre;
+        public final TextView mAvailibility;
+        public final TextView mBookOwner;
+        public Book mItem;
+
+        public BookViewHolder(View view) {
+            super(view);
+            mView = view;
+            mImageBook = mView.findViewById(R.id.imageViewBook_basic);
+            mTitle = mView.findViewById(R.id.textViewTitle);
+            mAuthor = mView.findViewById(R.id.textViewAuthor);
+            mBookRating = mView.findViewById(R.id.ratingBarBook);
+            mGenre = mView.findViewById(R.id.textViewGenre);
+            mAvailibility = mView.findViewById(R.id.textViewAvailibility);
+            mBookOwner = mView.findViewById(R.id.textViewBookOwner);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mItem.getBookTitle()+" "+mItem.getBookAuthor() + "'";
+        }
+    }
+
 }
