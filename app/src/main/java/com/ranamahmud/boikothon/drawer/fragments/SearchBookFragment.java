4d22f309953fc9fd68e2bca77230e4ad3c9407b2
Md Rana Mahmud <ranamd0@gmail.com>
2019-04-03 13:22:32 +0600

    Manage Book Requests Fragment Added

--- app/src/main/java/com/ranamahmud/boikothon/drawer/fragments/SearchBookFragment.java
+++ app/src/main/java/com/ranamahmud/boikothon/drawer/fragments/SearchBookFragment.java
@@ -1,12 +1,9 @@
 package com.ranamahmud.boikothon.drawer.fragments;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -19,10 +16,8 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.firestore.paging.FirestorePagingAdapter;
 import com.firebase.ui.firestore.paging.FirestorePagingOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -31,16 +26,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.like.LikeButton;
 import com.like.OnLikeListener;
-import com.ranamahmud.boikothon.MainActivity;
 import com.ranamahmud.boikothon.R;
 import com.ranamahmud.boikothon.model.Book;
 import com.squareup.picasso.Picasso;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -48,12 +40,9 @@ import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.paging.PagedList;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import static android.app.Activity.RESULT_OK;
 
 /**
  * A fragment representing a list of Items.
@@ -73,36 +62,7 @@ public class SearchBookFragment extends Fragment {
     private FirebaseUser currentUser;
     private boolean loggedInStatus;
     private int RC_SIGN_IN = 123;
-    private String name;
-    private String email;
-    private Uri photoUrl;
-    private String uid;
     private static final String TAG = "SearchBookFragment";
-    private Class fragmentClass;
-    private Fragment fragment;
-    private FloatingActionButton fab;
-    private static final int GALLERY = 1;
-    private static final int CAMERA = 2;
-    private NavigationView navigationView;
-    private Menu menu;
-    private Object IMAGE_DIRECTORY = "boikothon";
-    private AlertDialog dialogCreate;
-    private ImageView mImageViewProfile;
-    private TextView mName;
-    private TextView mEmail;
-    private MenuItem menuSearchBook;
-    private MenuItem menuGivenBook;
-    private MenuItem menuTakenBook;
-    private MenuItem menuWishList;
-    private MenuItem menuManageBook;
-    private MenuItem menuProfile;
-    private MenuItem menuSignIn;
-    private MenuItem menuSignOut;
-    private StorageReference mStorageRef;
-    private String bookTitle;
-    private String bookGenre;
-    private FirebaseFirestore firebaseFirestore;
-    private String bookWriter;
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
@@ -363,88 +323,7 @@ public class SearchBookFragment extends Fragment {
         // [END auth_fui_create_intent]
     }
 
-    // [START auth_fui_result]
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // code for image picker
-        if (resultCode == getActivity().RESULT_CANCELED) {
-            return;
-        }
-
-
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            if (resultCode == RESULT_OK) {
-                // Successfully signed in
-                setUserProfile();
-            } else {
-                Toast.makeText(getActivity(), "Try again please", Toast.LENGTH_SHORT).show();
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
-            }
-        }
-    }
-    // [END auth_fui_result]
-
-    //     user profile related functions
-    private void setUserProfile() {
-        navigationView = getActivity().findViewById(R.id.nav_view);
-        // Name, email address, and profile photo Url,uid
-        currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        name = currentUser.getDisplayName();
-        email = currentUser.getEmail();
-        photoUrl = currentUser.getPhotoUrl();
-        uid = currentUser.getUid();
-        // save info to shared preference
-        // Check if user's email is verified
-        boolean emailVerified = currentUser.isEmailVerified();
 
 
 
-
-        mName   = navigationView.getHeaderView(0).findViewById(R.id.header_name);
-        mEmail   = navigationView.getHeaderView(0).findViewById(R.id.header_email);
-        mImageViewProfile = navigationView.getHeaderView(0).findViewById(R.id.imageViewProfilePic);
-        //Fetch values from you database child and set it to the specific view object.
-        mName.setText(name);
-        mEmail.setText(email);
-        Picasso.get().load(photoUrl).into(mImageViewProfile);
-
-        fragment = new SearchBookFragment();
-
-        // Insert the fragment by replacing any existing fragment
-        FragmentManager fragmentManager =getFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.fragment_layout, fragment).commit();
-        // get the menu items
-
-        menu = navigationView.getMenu();
-        menuSearchBook = menu.findItem(R.id.nav_book_search);
-        menuGivenBook = menu.findItem(R.id.nav_book_given);
-        menuTakenBook = menu.findItem(R.id.nav_book_taken);
-        menuWishList = menu.findItem(R.id.nav_book_wish);
-        menuManageBook = menu.findItem(R.id.nav_book_manage);
-        menuProfile = menu.findItem(R.id.nav_profile);
-
-        menuSignIn =   menu.findItem(R.id.nav_sign_in);
-        menuSignOut =   menu.findItem(R.id.nav_sign_out);
-
-        menuSearchBook.setChecked(true);
-        // hide unnecessary menu items
-        menuGivenBook.setVisible(true);
-        menuTakenBook.setVisible(true);
-        menuWishList.setVisible(true);
-        menuManageBook.setVisible(true);
-        menuProfile.setVisible(true);
-        menuSignIn.setVisible(false);
-        menuSignOut.setVisible(true);
-
-
-        // get firebase storage reference
-        mStorageRef = FirebaseStorage.getInstance().getReference();
-    }
 }
